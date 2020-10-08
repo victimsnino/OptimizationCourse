81393b0b97f60bd90ca849b441f400aff83c57e6
Aleksey Loginov <victimsnino@gmail.com>
2020-10-09 00:27:30 +0300

    fix

--- solver.py
+++ solver.py
@@ -1,4 +1,11 @@
 import cplex
+import itertools
+
+def is_all_independent(matrix, indexes):
+    for i,j in  itertools.combinations(indexes, 2):
+        if matrix[i,j] == 1 or matrix[j,i] == 1:
+            return False
+    return True
 
 class Solver:
     def __init__(self):
@@ -9,16 +16,26 @@ class Solver:
         size = matrix.shape[0]
         self.add_variables(size)
 
+        for i,j in itertools.combinations(range(size), 2):
+                if matrix[j, i] == 0 and i != j:
+                    self.add_constraint([j, i], '<=', 1)
+
+        return
         for i in range(size):
-            for j in range(size):
-                if matrix[i, j] == 0 and i != j:
-                    self.add_constraint([i, j], '<=', 1)
+            indexes = [j for j in range(size) if matrix[i, j] == 0 and i != j]
+            for length in range(2, min(4, len(indexes))):
+                for combination in itertools.combinations(indexes, length):
+                    target = [i]+list(combination)
+                    if not is_all_independent(matrix, target):
+                        continue
+
+                    self.add_constraint(target, '<=', 1)
 
     def add_variables(self, count):
         self.__model.variables.add(obj=[1]*count,
                                    lb=[0]*count, 
                                    ub=[1]*count,
-                                   types=[self.__model.variables.type.integer]*count)
+                                   types=[self.__model.variables.type.binary]*count)
 
     def add_constraint(self, indexes_or_variables, sense, value, multiples_for_indexes=None):
         valid_operations = ["<=", ">=", "=="]
@@ -37,9 +54,10 @@ class Solver:
 
     def solve(self):
         self.__model.set_results_stream(None)
+        self.__model.write("example.lp")
         self.__model.solve()
 
         values = self.__model.solution.get_values()
         
-        clique_points = [index for index, val in enumerate(values) if val == 1]
+        clique_points = [index for index, val in enumerate(values) if val > 0]
         return clique_points
--- tests.py
+++ tests.py
@@ -6,15 +6,15 @@ import numpy as np
 
 def test_simple_clique():
     solver = Solver()
-    matrix = np.zeros((6,6))
-    edges = [[0,1], [0,2], [1,2], [3,0],[4,1], [5,2]]
+    matrix = np.zeros((7,7))
+    edges = [[0,1], [0,2], [1,2], [3,0],[4,1], [5,2], [6,0],[6,1],[6,2], [6,3], [6,4]]
     for i,j in edges:
         matrix[i,j] = 1
         matrix[j,i] = 1
 
     solver.fill_from_matrix(matrix)
 
-    assert solver.solve() == [0, 1, 2]
+    assert solver.solve() == [0, 1, 2, 6]
 
 
 @pytest.mark.parametrize("file_name", ["brock200_2"]) #"C125.9", "keller4", "brock200_2", "p_hat300-1"])
