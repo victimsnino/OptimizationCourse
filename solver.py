fb8da69a85133b2ca2b487a0b4a955e062e6a383
Aleksey Loginov <victimsnino@gmail.com>
2020-10-11 15:04:44 +0300

    more speed

--- solver.py
+++ solver.py
@@ -5,13 +5,17 @@ from numpy.matrixlib.defmatrix import matrix
 from utils import *
 import random
 
-def maximal_independent_set(matrix, node):
+# Find maximal independent set for target node. Moreover, 
+# it search's only in rest part of matrix
+# (not 0..n, 0..n, but in node...n, node...n matrix)
+def maximal_independent_set(matrix, input_node):
     size = matrix.shape[0]
-    find_neighbours = lambda x: set([j for j in range(size) if matrix[node, j] == 1])
+    node = input_node
+    find_neighbours = lambda x: set([j for j in range(input_node, size) if matrix[x, j] == 1])
     neighbors = find_neighbours(node)
  
     indep_nodes = [node]
-    not_neighbours = set(range(size)).difference(neighbors.union(indep_nodes))
+    not_neighbours = set(range(input_node,size)).difference(neighbors.union(indep_nodes))
  
     while not_neighbours:
         node = random.choice(list(not_neighbours))
@@ -22,10 +26,11 @@ def maximal_independent_set(matrix, node):
     return indep_nodes
 
 class Solver:
-    def __init__(self, binary=True):
+    def __init__(self, binary=True, independent_set =True):
         self.__model = cplex.Cplex()
         self.__model.objective.set_sense(self.__model.objective.sense.maximize)
         self.__binary = binary
+        self.__independent_set = independent_set
 
     def fill_from_matrix(self, matrix):
         size = matrix.shape[0]
@@ -35,7 +40,9 @@ class Solver:
             for j in range(i+1,size):
                 if matrix[i, j] == 0:
                     self.add_constraint([j, i], '<=', 1)
-            self.add_constraint(maximal_independent_set(matrix, i), '<=', 1)
+
+            if self.__independent_set:
+                self.add_constraint(maximal_independent_set(matrix, i), '<=', 1)
 
 
     def add_variables(self, count):
